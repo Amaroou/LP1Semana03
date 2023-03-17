e3bee60315157ef84f41d994a7f3069ff7ee17a1
João Amaro <Jamaro2110@gmail.com>
2023-03-17 10:55:25 +0000

    Exercicio 1

---
+++ ChangeString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string - ");
+            string teststring = Console.ReadLine();
+            Console.WriteLine("Enter a character - ");
+            string character = Console.ReadLine();
+            var str = teststring;
+            var charstoremove = new string[] {character};
+            foreach (var c in charstoremove)
+            {
+                str = str.Replace(character, string.Empty);
+            }
+            Console.WriteLine(str);
+        }
+
+
+    }
+}
